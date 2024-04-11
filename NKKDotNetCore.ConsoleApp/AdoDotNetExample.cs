e89409fadc68f95f688e06ebdb3bc0791b2d0f4e
naykhantkyaw-coding <naykhantkyaw.coding@gmail.com>
2024-04-11 22:53:13 +0630

    [Complete] Replacing Read method to AdoDotNetExample

---
+++ NKKDotNetCore.ConsoleApp/AdoDotNetExample.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NKKDotNetCore.ConsoleApp
+{
+    public class AdoDotNetExample
+    {
+        public readonly SqlConnectionStringBuilder _stringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".", // server name
+            InitialCatalog = "DotNetCoreTrainingBatch4", // Db name
+            UserID = "sa",
+            Password = "sasa@123"
+        };
+        public void Read()
+        {
+            SqlConnection connection = new SqlConnection(_stringBuilder.ConnectionString);
+
+            connection.Open();
+            Console.WriteLine("Connection.Open");
+
+            string query = "select * from BlogTable";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            connection.Close();
+            Console.WriteLine("Connection.Close");
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                Console.WriteLine($"Id => {dr["BlogId"]}");
+                Console.WriteLine($"Title => {dr["BlogTitle"]}");
+                Console.WriteLine($"Author => {dr["BlogAuthor"]}");
+                Console.WriteLine($"Content => {dr["BlogContent"]}");
+                Console.WriteLine("------------------------");
+            }
+        }
+
+    }
+}
--- NKKDotNetCore.ConsoleApp/Program.cs
+++ NKKDotNetCore.ConsoleApp/Program.cs
@@ -1,34 +1,39 @@
-﻿using System.Data;
+﻿using NKKDotNetCore.ConsoleApp;
+using System.Data;
 using System.Data.SqlClient;
 using System.Text;
 
 Console.WriteLine("Hello, World!");
 
-SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
-stringBuilder.DataSource = "."; // server name
-stringBuilder.InitialCatalog = "DotNetCoreTrainingBatch4"; // Db name
-stringBuilder.UserID = "sa";
-stringBuilder.Password = "sa@123";
-SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+//SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+//stringBuilder.DataSource = "."; // server name
+//stringBuilder.InitialCatalog = "DotNetCoreTrainingBatch4"; // Db name
+//stringBuilder.UserID = "sa";
+//stringBuilder.Password = "sasa@123";
+//SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
 
-connection.Open();
-Console.WriteLine("Connection.Open");
+//connection.Open();
+//Console.WriteLine("Connection.Open");
 
-string query = "select * from BlogTable";
-SqlCommand cmd = new SqlCommand(query, connection);
-SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-DataTable dt = new DataTable();
-adapter.Fill(dt);
+//string query = "select * from BlogTable";
+//SqlCommand cmd = new SqlCommand(query, connection);
+//SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+//DataTable dt = new DataTable();
+//adapter.Fill(dt);
 
-connection.Close();
-Console.WriteLine("Connection.Close");
+//connection.Close();
+//Console.WriteLine("Connection.Close");
 
-foreach (DataRow dr in dt.Rows)
-{
-    Console.WriteLine(dr["BlogId"]);
-    Console.WriteLine(dr["BlogAuthor"]);
-    Console.WriteLine(dr["BlogContent"]);
-    Console.WriteLine("------------------------");
-}
+//foreach (DataRow dr in dt.Rows)
+//{
+//    Console.WriteLine(dr["BlogId"]);
+//    Console.WriteLine(dr["BlogTitle"]);
+//    Console.WriteLine(dr["BlogAuthor"]);
+//    Console.WriteLine(dr["BlogContent"]);
+//    Console.WriteLine("------------------------");
+//}
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+adoDotNetExample.Read();
 
 Console.ReadKey();
