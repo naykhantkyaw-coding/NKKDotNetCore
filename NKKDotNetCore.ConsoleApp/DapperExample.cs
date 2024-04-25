1243d1265d265050d77d7a6b7bff83d1cddf13a5
naykhantkyaw-coding <naykhantkyaw.coding@gmail.com>
2024-04-25 23:13:48 +0630

    [Complete] Blog Read with Dapper [Complete] Blog Edit With Dapper

--- NKKDotNetCore.ConsoleApp/DapperExample.cs
+++ NKKDotNetCore.ConsoleApp/DapperExample.cs
@@ -15,7 +15,26 @@ namespace NKKDotNetCore.ConsoleApp
     {
         public void Run()
         {
-            Create("TestingDapper", "TestingDP", "TestingDp");
+            Read();
+            //Create("TestingDapper", "TestingDP", "TestingDp");
+            Edit(1);
+            Edit(3000);
+        }
+
+        private void Read()
+        {
+            using IDbConnection db = new SqlConnection(ConnectionStrings.connectionString.ConnectionString);
+            db.Open();
+            List<BlogModel> model = db.Query<BlogModel>("select * from BlogTable").ToList();
+
+            foreach (BlogModel item in model)
+            {
+                Console.WriteLine($"BlogID : {item.BlogId}");
+                Console.WriteLine($"BlogTitle : {item.BlogTitle}");
+                Console.WriteLine($"BlogAuthor : {item.BlogAuthor}");
+                Console.WriteLine($"BlogContent : {item.BlogContent}");
+                Console.WriteLine("-------------------------------");
+            }
         }
 
         private void Create(string blogTitle, string blogAuthor, string blogContent)
@@ -39,6 +58,26 @@ namespace NKKDotNetCore.ConsoleApp
             var result = db.Execute(query, item);
             string message = result > 0 ? "Create successful" : "Create fail.";
             Console.WriteLine(message);
+        }
+
+        private void Edit(int id)
+        {
+            IDbConnection db = new SqlConnection(ConnectionStrings.connectionString.ConnectionString);
+            string message = string.Empty;
+            db.Open();
+            //BlogModel? model = db.Query<BlogModel>("select * from BlogTable where BlogId=@BlogId ", id).FirstOrDefault(); // error scalar variable declear
+            BlogModel? model = db.Query<BlogModel>("select * from BlogTable where BlogId=@BlogId ", new BlogModel { BlogId = id })
+                .FirstOrDefault();
+            if (model == null)
+            {
+                message = "No Data Found.";
+            }
+
+            Console.WriteLine($"BlogID : {model.BlogId}");
+            Console.WriteLine($"BlogTitle : {model.BlogTitle}");
+            Console.WriteLine($"BlogAuthor : {model.BlogAuthor}");
+            Console.WriteLine($"BlogContent : {model.BlogContent}");
+            Console.WriteLine("-------------------------------");
         }
 
     }
