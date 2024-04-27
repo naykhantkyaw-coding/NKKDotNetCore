4b49159651d670eb57b7c135c2c3433e4b0bd1e9
naykhantkyaw-coding <naykhantkyaw.coding@gmail.com>
2024-04-27 20:57:14 +0630

    [Complete] Complete Api Get

---
+++ NKKDotNetCore.RestApi/Controllers/EFCoreExample/BlogController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NKKDotNetCore.RestApi.DbContexts;
+
+namespace NKKDotNetCore.RestApi.Controllers.EFCoreExample
+{
+    // domain https://localhost:7295 
+    //end point ai/blog
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+        public BlogController()
+        {
+            _appDbContext = new AppDbContext();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var lst = _appDbContext.Blogs.ToList();
+            return Ok(lst);
+        }
+
+
+    }
+}
