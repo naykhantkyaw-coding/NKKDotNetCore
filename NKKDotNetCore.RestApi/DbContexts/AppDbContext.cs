3e85764d46956c76da0ab0bcaa58abcc0c02b268
naykhantkyaw-coding <naykhantkyaw.coding@gmail.com>
2024-04-27 20:25:17 +0630

    [Complete] Adding Requrie file to connect DB with EFCore

---
+++ NKKDotNetCore.RestApi/DbContexts/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NKKDotNetCore.RestApi.Services;
+using NKKDotNetCore.RestApi.Model;
+
+namespace NKKDotNetCore.RestApi.DbContexts
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionStrings.connectionString.ConnectionString);
+        }
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
---
+++ NKKDotNetCore.RestApi/Services/ConnectionStrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NKKDotNetCore.RestApi.Services
+{
+    internal static class ConnectionStrings
+    {
+        public static SqlConnectionStringBuilder connectionString = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "DotNetCoreTrainingBatch4",
+            UserID = "sa",
+            Password = "sasa@123",
+            TrustServerCertificate = true,
+        };
+    }
+}
