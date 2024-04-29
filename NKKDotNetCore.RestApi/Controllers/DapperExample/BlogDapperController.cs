c11917b14090444efeaa875bbf9910f08f45338b
naykhantkyaw-coding <naykhantkyaw.coding@gmail.com>
2024-04-29 22:47:34 +0630

    [Complete] Adding Dapper Example

---
+++ NKKDotNetCore.RestApi/Controllers/DapperExample/BlogDapperController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NKKDotNetCore.RestApi.Controllers.DapperExample
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogDapperController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetBlog()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBlogByID(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlog()
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBlog(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPatch]
+        public IActionResult PatchUpdateBlog(int id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBlog(int id)
+        {
+            return Ok();
+        }
+    }
+}
