8b109e4c491fc5484f57381b1bb2b81e716065bf
naykhantkyaw-coding <naykhantkyaw.coding@gmail.com>
2024-05-07 23:49:17 +0630

    [Complete] Adding AdoDotNetController for Api

---
+++ NKKDotNetCore.RestApi/Controllers/AdoDotNetExample/AdoDotNetController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NKKDotNetCore.RestApi.Controllers.AdoDotNetExample
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdoDotNetController : ControllerBase
+    {
+    }
+}
