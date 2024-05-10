d861fc85368fec596fb697d3f13083245e0d3123
naykhantkyaw-coding <naykhantkyaw.coding@gmail.com>
2024-05-10 23:31:29 +0630

    [Complete] Dapper query Service

--- NKKDotNetCore.Shared/DapperService.cs
+++ NKKDotNetCore.Shared/DapperService.cs
@@ -1,7 +1,20 @@
-﻿namespace NKKDotNetCore.Shared
+﻿using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace NKKDotNetCore.Shared
 {
     public class DapperService
     {
-
+        public List<T> GetData<T>(string? query)
+        {
+            using IDbConnection connection = new SqlConnection();
+            List<T> data = new List<T>();
+            if (query is not null)
+            {
+                data = connection.Query<T>(query).ToList();
+            }
+            return data;
+        }
     }
 }
