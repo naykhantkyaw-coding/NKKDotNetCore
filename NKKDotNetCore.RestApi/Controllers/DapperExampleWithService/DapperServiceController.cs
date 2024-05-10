f8e5ff4ab6cf4391351ab42c66bddc4529cb58e1
naykhantkyaw-coding <naykhantkyaw.coding@gmail.com>
2024-05-11 00:08:54 +0630

    [Complete] GetBlogs Api with Dapper Service

---
+++ NKKDotNetCore.RestApi/Controllers/DapperExampleWithService/DapperServiceController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NKKDotNetCore.RestApi.Model;
+using NKKDotNetCore.Shared.Services;
+
+namespace NKKDotNetCore.RestApi.Controllers.DapperExampleWithService
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DapperServiceController : ControllerBase
+    {
+        private readonly DapperService _dapperService = new DapperService();
+
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            string query = "select * from BlogTable";
+            var lst = _dapperService.GetData<BlogModel>(query);
+            return Ok(lst);
+        }
+    }
+}
