ff443cd19d0db9287df669ee2d821c75537644e3
naykhantkyaw-coding <naykhantkyaw.coding@gmail.com>
2024-05-11 22:59:41 +0630

    [Complete] Adding AdoDotNetService Controller

---
+++ NKKDotNetCore.RestApi/Controllers/AdoDotNetExampleWithService/AdoDotNetServiceController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NKKDotNetCore.Shared.Services;
+
+namespace NKKDotNetCore.RestApi.Controllers.AdoDotNetExampleWithService
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdoDotNetServiceController : ControllerBase
+    {
+        private readonly AdoDotNetService _service = new AdoDotNetService();
+    }
+}
