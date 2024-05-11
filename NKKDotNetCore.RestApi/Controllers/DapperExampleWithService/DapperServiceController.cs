5f9e0852fc07edfa970d36bdc2cceadf483cd7ed
naykhantkyaw-coding <naykhantkyaw.coding@gmail.com>
2024-05-11 21:43:33 +0630

    [Complete] BlogPatchUpdate with dapperservice

--- NKKDotNetCore.RestApi/Controllers/DapperExampleWithService/DapperServiceController.cs
+++ NKKDotNetCore.RestApi/Controllers/DapperExampleWithService/DapperServiceController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using NKKDotNetCore.RestApi.Model;
 using NKKDotNetCore.Shared.Services;
 
@@ -57,8 +58,44 @@ namespace NKKDotNetCore.RestApi.Controllers.DapperExampleWithService
               ,[BlogAuthor] = @BlogAuthor
               ,[BlogContent] = @BlogContent
              WHERE BlogId = @BlogId";
+            model.BlogId = id;
             var result = _dapperService.Execute(query, model);
             string message = result > 0 ? "Update successful" : "Update fail.";
+            return Ok(message);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult PatchUpdate(int id, BlogModel model)
+        {
+            var item = FindById(id);
+            if (item is null)
+            {
+                return NotFound("No data found.");
+            }
+            string condition = string.Empty;
+            if (!model.BlogTitle.IsNullOrEmpty())
+            {
+                condition += " [BlogTitle] = @BlogTitle, ";
+            }
+            if (!model.BlogAuthor.IsNullOrEmpty())
+            {
+                condition += " [BlogAuthor] = @BlogAuthor, ";
+            }
+            if (!model.BlogContent.IsNullOrEmpty())
+            {
+                condition += " [BlogContent] = @BlogContent, ";
+            }
+            if (condition.Length == 0)
+            {
+                return NotFound("No data found.");
+            }
+            condition = condition.Substring(0, condition.Length - 2);
+            model.BlogId = id;
+            string query = $@"UPDATE [dbo].[BlogTable]
+            SET {condition}
+             WHERE BlogId = @BlogId";
+            var result = _dapperService.Execute(query, model);
+            string message = result > 0 ? "PatchUpdate successful" : "PatchUpdate fail.";
             return Ok(message);
         }
 
