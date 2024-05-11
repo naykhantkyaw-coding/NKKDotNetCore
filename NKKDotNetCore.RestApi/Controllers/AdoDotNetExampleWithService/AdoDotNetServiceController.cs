33faa8efa292f08b4197d6d4d64311b1118f6183
naykhantkyaw-coding <naykhantkyaw.coding@gmail.com>
2024-05-11 23:37:52 +0630

    [Complete] CreateBlog with AdoDotNet Service

--- NKKDotNetCore.RestApi/Controllers/AdoDotNetExampleWithService/AdoDotNetServiceController.cs
+++ NKKDotNetCore.RestApi/Controllers/AdoDotNetExampleWithService/AdoDotNetServiceController.cs
@@ -26,5 +26,35 @@ namespace NKKDotNetCore.RestApi.Controllers.AdoDotNetExampleWithService
             var result = _service.QueryFirstOrDefault<BlogModel>(query, new AdoDotNetParamters("@BlogId", id));
             return Ok(result);
         }
+
+        [HttpPost]
+        public IActionResult CreateBlog(BlogModel model)
+        {
+            string query = @"INSERT INTO [dbo].[BlogTable]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent])
+     VALUES
+           (@BlogTitle
+           ,@BlogAuthor
+           ,@BlogContent)";
+            var result = _service.Execute(query,
+                new AdoDotNetParamters("@BlogTitle", model.BlogTitle),
+                new AdoDotNetParamters("@BlogAuthor", model.BlogAuthor),
+                new AdoDotNetParamters("@BlogContent", model.BlogContent));
+            string message = result > 0 ? "Create Success." : "Create fail.";
+            return Ok(message);
+        }
+
+        private BlogModel GetById(int id)
+        {
+            string query = "select * from BlogTable where BlogId = @BlogId";
+            BlogModel? result = _service.QueryFirstOrDefault<BlogModel>(query, new AdoDotNetParamters("@BlogId", id));
+            if (result is null)
+            {
+                return null;
+            }
+            return result;
+        }
     }
 }
