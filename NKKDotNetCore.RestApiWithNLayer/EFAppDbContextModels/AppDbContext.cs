1a664ed68337ffe00d337f920e84673006bd157f
naykhantkyaw-coding <naykhantkyaw.coding@gmail.com>
2024-05-13 22:20:57 +0630

    [Complete] Scaffold Database

---
+++ NKKDotNetCore.RestApiWithNLayer/EFAppDbContextModels/AppDbContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace NKKDotNetCore.RestApiWithNLayer.EFAppDbContextModels;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<BlogTable> BlogTables { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<BlogTable>(entity =>
+        {
+            entity.HasKey(e => e.BlogId);
+
+            entity.ToTable("BlogTable");
+
+            entity.Property(e => e.BlogAuthor)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.BlogContent)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.BlogTitle)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ NKKDotNetCore.RestApiWithNLayer/EFAppDbContextModels/BlogTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NKKDotNetCore.RestApiWithNLayer.EFAppDbContextModels;
+
+public partial class BlogTable
+{
+    public int BlogId { get; set; }
+
+    public string BlogTitle { get; set; } = null!;
+
+    public string BlogAuthor { get; set; } = null!;
+
+    public string BlogContent { get; set; } = null!;
+}
