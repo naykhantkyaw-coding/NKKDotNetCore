a5a9fb63f9af37281bafb98d086a383e380dc6b1
naykhantkyaw-coding <naykhantkyaw.coding@gmail.com>
2024-05-13 23:40:54 +0630

    [Complete] DA updateblog complete

--- NKKDotNetCore.RestApiWithNLayer/Features/Blog/DA_Blog.cs
+++ NKKDotNetCore.RestApiWithNLayer/Features/Blog/DA_Blog.cs
@@ -49,5 +49,30 @@ namespace NKKDotNetCore.RestApiWithNLayer.Features.Blog
             var result = _dbContext.SaveChanges();
             return result > 0 ? "Create success." : "Create fail.";
         }
+
+        public string UpdateBlog(int id, BlogTable model)
+        {
+            int result = 0;
+            try
+            {
+                var item = _dbContext.BlogTables.Find(id);
+                if (item is null)
+                {
+                    result = 0;
+                }
+                item.BlogTitle = model.BlogTitle;
+                item.BlogAuthor = model.BlogAuthor;
+                item.BlogContent = model.BlogContent;
+                _dbContext.Update(item);
+                result = _dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return result > 0 ? "Update success." : "Update fail.";
+
+        }
     }
 }
