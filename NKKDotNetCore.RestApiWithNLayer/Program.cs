0380ada73a197c5c435271df90fe6b56e659b05e
naykhantkyaw-coding <naykhantkyaw.coding@gmail.com>
2024-05-13 22:34:22 +0630

    [Comlete] Adding DI

--- NKKDotNetCore.RestApiWithNLayer/Program.cs
+++ NKKDotNetCore.RestApiWithNLayer/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using NKKDotNetCore.RestApiWithNLayer.EFAppDbContextModels;
+using NKKDotNetCore.RestApiWithNLayer.Features.Blog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region AddDbContext
+
+builder.Services.AddDbContext<AppDbContext>(
+opt => opt.UseSqlServer(builder.Configuration.GetSection("CustomSetting:ConnectionStrings:DbConnection").Value),
+ServiceLifetime.Transient,
+ServiceLifetime.Transient
+);
+
+#endregion
+
+#region Add DI
+
+builder.Services.AddScoped<BL_Blog>();
+builder.Services.AddScoped<DA_Blog>();
+
+#endregion
 
 var app = builder.Build();
 
