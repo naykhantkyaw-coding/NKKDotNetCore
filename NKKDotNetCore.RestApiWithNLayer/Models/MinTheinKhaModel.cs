fdbeb4f291335610a114c7bb0eee72ca124e5fbd
naykhantkyaw-coding <naykhantkyaw.coding@gmail.com>
2024-05-14 23:43:13 +0630

    Fixing MinTheinKhaModel

--- NKKDotNetCore.RestApiWithNLayer/Models/MinTheinKhaModel.cs
+++ NKKDotNetCore.RestApiWithNLayer/Models/MinTheinKhaModel.cs
@@ -2,20 +2,20 @@
 
 public class MeinTheinKhaModel
 {
-    public Question[] questions { get; set; }
-    public Answer[] answers { get; set; }
-    public string[] numberList { get; set; }
+    public List<Question>? questions { get; set; }
+    public List<Answer>? answers { get; set; }
+    public List<string>? numberList { get; set; }
 }
 
 public class Question
 {
     public int questionNo { get; set; }
-    public string questionName { get; set; }
+    public string? questionName { get; set; }
 }
 
 public class Answer
 {
     public int questionNo { get; set; }
     public int answerNo { get; set; }
-    public string answerResult { get; set; }
+    public string? answerResult { get; set; }
 }
