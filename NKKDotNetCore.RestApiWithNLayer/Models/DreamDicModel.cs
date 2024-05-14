f6fc2d10a1e4994b0e3ab87cfa385bf91c014060
naykhantkyaw-coding <naykhantkyaw.coding@gmail.com>
2024-05-15 02:14:27 +0630

    [Complete] Adding data and Model

---
+++ NKKDotNetCore.RestApiWithNLayer/Models/DreamDicModel.cs
@@ -0,0 +1,19 @@
+﻿namespace NKKDotNetCore.RestApiWithNLayer.Models;
+public class DreamDicModel
+{
+    public List<Blogheader>? BlogHeader { get; set; }
+    public List<Blogdetail>? BlogDetail { get; set; }
+}
+
+public class Blogheader
+{
+    public int BlogId { get; set; }
+    public string? BlogTitle { get; set; }
+}
+
+public class Blogdetail
+{
+    public int BlogDetailId { get; set; }
+    public int BlogId { get; set; }
+    public string? BlogContent { get; set; }
+}
