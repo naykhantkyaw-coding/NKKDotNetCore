4627a1f43b462915f912b0d8d2f224e2d7c6e953
naykhantkyaw-coding <naykhantkyaw.coding@gmail.com>
2024-05-15 02:17:20 +0630

    [Complete] DA_DreamDic

---
+++ NKKDotNetCore.RestApiWithNLayer/Features/DreamDic/DA_DreamDic.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using NKKDotNetCore.RestApiWithNLayer.Models;
+
+namespace NKKDotNetCore.RestApiWithNLayer.Features.DreamDic
+{
+    public class DA_DreamDic
+    {
+        public async Task<DreamDicModel?> GetAllData()
+        {
+            var jsonStr = await File.ReadAllTextAsync("dream-dic.json");
+            var model = JsonConvert.DeserializeObject<DreamDicModel>(jsonStr);
+            return model;
+        }
+    }
+}
