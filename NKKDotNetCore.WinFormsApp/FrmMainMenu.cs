2b41527025890ffa5ec10427115884b082622f1b
naykhantkyaw-coding <naykhantkyaw.coding@gmail.com>
2024-06-03 23:25:04 +0630

    [Complete] Adding MainMenu form

---
+++ NKKDotNetCore.WinFormsApp/FrmMainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NKKDotNetCore.WinFormsApp
+{
+    public partial class FrmMainMenu : Form
+    {
+        public FrmMainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void newBlogToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmBlog frmBlog = new FrmBlog();
+            frmBlog.ShowDialog();
+        }
+    }
+}
