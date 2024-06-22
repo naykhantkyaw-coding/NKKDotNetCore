a81b9cc7b91d17f992cb2464fb98beff0a3adb09
naykhantkyaw-coding <naykhantkyaw.coding@gmail.com>
2024-06-22 13:55:23 +0630

    [Complete] Createing Project

---
+++ NKKDotNetCore.WinFormsAppSqlInjection/Form1.cs
@@ -0,0 +1,10 @@
+namespace NKKDotNetCore.WinFormsAppSqlInjection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
