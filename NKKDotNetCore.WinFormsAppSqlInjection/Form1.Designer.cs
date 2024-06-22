16d6356d2b9ca94494379f008fb4dcee0efb6bdc
naykhantkyaw-coding <naykhantkyaw.coding@gmail.com>
2024-06-22 14:14:05 +0630

    [Complete] Createing Design

--- NKKDotNetCore.WinFormsAppSqlInjection/Form1.Designer.cs
+++ NKKDotNetCore.WinFormsAppSqlInjection/Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            txtEmail = new TextBox();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            lblEmail = new Label();
+            lblPassword = new Label();
+            SuspendLayout();
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(144, 80);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(313, 27);
+            txtEmail.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(144, 133);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(313, 27);
+            txtPassword.TabIndex = 1;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.FromArgb(0, 192, 0);
+            btnLogin.ForeColor = SystemColors.ActiveCaptionText;
+            btnLogin.Location = new Point(144, 177);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(94, 36);
+            btnLogin.TabIndex = 2;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // lblEmail
+            // 
+            lblEmail.AutoSize = true;
+            lblEmail.Location = new Point(36, 87);
+            lblEmail.Name = "lblEmail";
+            lblEmail.Size = new Size(46, 20);
+            lblEmail.TabIndex = 3;
+            lblEmail.Text = "Email";
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Location = new Point(36, 140);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(70, 20);
+            lblPassword.TabIndex = 4;
+            lblPassword.Text = "Password";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(585, 294);
+            Controls.Add(lblPassword);
+            Controls.Add(lblEmail);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtEmail);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtEmail;
+        private TextBox txtPassword;
+        private Button btnLogin;
+        private Label lblEmail;
+        private Label lblPassword;
     }
 }
