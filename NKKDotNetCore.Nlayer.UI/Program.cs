65fbb2abe3ce935f37e1c80b22a56e55115108ce
naykhantkyaw-coding <naykhantkyaw.coding@gmail.com>
2024-06-23 11:35:49 +0630

    [Complete] Nlayer

--- NKKDotNetCore.Nlayer.UI/Program.cs
+++ NKKDotNetCore.Nlayer.UI/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using NKKDotNetCore.Nlayer.BusinessLogic.Services;
+
 Console.WriteLine("Hello, World!");
+
+BL_Blog blog = new BL_Blog();
+var data = blog.GetAllData();
+
+foreach (var item in data)
+{
+    Console.WriteLine($"Blog ID => {item.BlogId}");
+    Console.WriteLine($"Blog BlogTitle => {item.BlogTitle}");
+    Console.WriteLine($"Blog BlogAuthor => {item.BlogAuthor}");
+    Console.WriteLine($"Blog BlogContent => {item.BlogContent}");
+}
